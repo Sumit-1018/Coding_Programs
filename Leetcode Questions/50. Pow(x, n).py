082c70ca3b5f8d97759879ae9f0d63753bc33642
Sumit <sumitgupta.1018@gmail.com>
2023-08-06 20:14:51 +0530

    Question 50

---
+++ Leetcode Questions/50. Pow(x, n).py
@@ -0,0 +1,9 @@
+import math
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return math.pow(x,n)
