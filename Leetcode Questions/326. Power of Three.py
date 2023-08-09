e39fc6e42f3d887d1c56d11bf13e532e0eb238b2
Sumit <sumitgupta.1018@gmail.com>
2023-08-09 21:27:58 +0530

    Question 326

---
+++ Leetcode Questions/326. Power of Three.py
@@ -0,0 +1,13 @@
+import math
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
+        if n==1:
+            return True
+        if n==0 or n%3!=0:
+            return False
+        return Solution.isPowerOfThree(self,n/3)
