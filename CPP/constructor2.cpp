bf4d2cb8b575ccfa670e1d38b7286fa52ab7088d
Sumit <sumitgupta.1018@gmail.com>
2023-09-04 20:43:16 +0530

    Constructors

---
+++ CPP/constructor2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle {
+private:
+    int rectLength;
+    int rectWidth;
+
+public:
+    Rectangle(int length, int width) {
+        rectLength = length;
+        rectWidth = width;
+    }
+
+    int getArea() {
+        return rectLength * rectWidth;
+    }
+
+    int getPerimeter() {
+        return 2 * (rectLength + rectWidth);
+    }
+};
+
+int main() {
+    Rectangle rect(10, 5);
+    cout << "Area: " << rect.getArea() << endl;
+    cout << "Perimeter: " << rect.getPerimeter() << endl;
+    return 0;
+}
